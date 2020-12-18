00ec9aefa5237956a8e6564200e4537f6e1a699a
unknown <yeodonghan@gmail.com>
2020-12-18 11:41:52 +0800

    Updated all settings routes

--- src/routes/settings/privacypolicy.svelte
+++ src/routes/settings/privacypolicy.svelte
@@ -4,23 +4,24 @@
   import Card from "sveltestrap/src/Card.svelte";
   import CardBody from "sveltestrap/src/CardBody.svelte";
   import CardHeader from "sveltestrap/src/CardHeader.svelte";
-
+  import FormGroup from "sveltestrap/src/FormGroup.svelte";
+  import Label from "sveltestrap/src/Label.svelte";
+  import Input from "sveltestrap/src/Input.svelte";
+  import Button from "sveltestrap/src/Button.svelte";
 </script>
 
 <h1 class="mt-4">Privacy Policy</h1>
 
 <Card class="mb-4">
-  <CardHeader>
-    Update Privacy Policy for app usage
-  </CardHeader>
+  <CardHeader>Update Privacy Policy for app usage</CardHeader>
   <CardBody>
-    DataTables is a third party plugin that is used to generate the demo table
-    below. For more information about DataTables, please visit the
-    <a target="_blank" href="https://datatables.net/">
-      official DataTables documentation
-    </a>
-    .
+    <FormGroup>
+      <Label for="exampleText" class="small mb-1">Privacy Policy</Label>
+      <Input type="textarea" name="text" id="exampleText" />
+    </FormGroup>
+
+    <hr />
+
+    <Button block color="primary">Update Policy</Button>
   </CardBody>
 </Card>
-
-
--- src/routes/settings/termsandconditions.svelte
+++ src/routes/settings/termsandconditions.svelte
@@ -4,51 +4,26 @@
   import Card from "sveltestrap/src/Card.svelte";
   import CardBody from "sveltestrap/src/CardBody.svelte";
   import CardHeader from "sveltestrap/src/CardHeader.svelte";
+  import FormGroup from "sveltestrap/src/FormGroup.svelte";
+  import Label from "sveltestrap/src/Label.svelte";
+  import Input from "sveltestrap/src/Input.svelte";
+  import Button from "sveltestrap/src/Button.svelte";
 
   import Table from "../../components/Table.svelte";
 </script>
 
-<h1 class="mt-4">Tables</h1>
-<Breadcrumb class="mb-4">
-  <BreadcrumbItem>
-    <a href=".">Dashboard</a>
-  </BreadcrumbItem>
-  <BreadcrumbItem active>Tables</BreadcrumbItem>
-</Breadcrumb>
+<h1 class="mt-4">Terms and Conditions</h1>
 
 <Card class="mb-4">
+  <CardHeader>Update Terms and Condition</CardHeader>
   <CardBody>
-    DataTables is a third party plugin that is used to generate the demo table
-    below. For more information about DataTables, please visit the
-    <a target="_blank" href="https://datatables.net/">
-      official DataTables documentation
-    </a>
-    .
-  </CardBody>
-</Card>
+    <FormGroup>
+      <Label for="exampleText" class="small mb-1">Terms and Conditions</Label>
+      <Input type="textarea" name="text" id="exampleText" />
+    </FormGroup>
 
-<Card class="mb-4">
-  <CardHeader>
-    <svg
-      class="svg-inline--fa fa-table fa-w-16"
-      aria-hidden="true"
-      focusable="false"
-      data-prefix="fas"
-      data-icon="table"
-      role="img"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 512 512"
-      data-fa-i2svg="">
-      <path
-        fill="currentColor"
-        d="M464 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h416c26.51
-        0 48-21.49 48-48V80c0-26.51-21.49-48-48-48zM224
-        416H64v-96h160v96zm0-160H64v-96h160v96zm224
-        160H288v-96h160v96zm0-160H288v-96h160v96z" />
-    </svg>
-    DataTable Example
-  </CardHeader>
-  <CardBody>
-    <Table />
+    <hr />
+
+    <Button block color="primary">Update Terms</Button>
   </CardBody>
 </Card>
